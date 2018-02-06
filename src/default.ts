961840b265e04ac9046c176dad40daf8cb4f96d2
Sam Noedel <sam.noedel@gmail.com>
2018-02-05 21:31:10 -0800

    Create TS 2.6 version

--- src/default.ts
+++
@@ -1,5 +0,0 @@
-import EventEmitter from 'eventemitter3'
-
-const emitter = new EventEmitter()
-emitter.on('some-event', () => console.log('default imports work!'))
-emitter.emit('some-event')
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
 import './named'
-import './default'
 import './import-equals'
